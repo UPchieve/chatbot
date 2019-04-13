37713936a2ac8a2d120713445bc32bb0d6dae9b6
Jake Romer <mail@jakeromer.com>
2019-04-13 18:45:34 +0000

    Add unicorn config

---
+++ config.ru
@@ -0,0 +1,6 @@
+require "rubygems"
+require "sinatra"
+
+require File.expand_path '../upchieve_chatbot.rb', __FILE__
+
+run UPchieveChatbot
---
+++ upchieve_chatbot.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "sinatra/base"
+
+class UPchieveChatbot < Sinatra::Base
+  post "/deploy" do
+    return "Unauthorized" if params[:key] != ENV["SLACK_WEBHOOK_KEY"]
+
+    environment = params[:command].to_s.scan(/deploy-(.+)/).flatten.first
+    args = params[:text].to_s.split
+    web = args.select { |arg| arg =~ %r{web/} }.first || "web/master"
+    server = args.select { |arg| arg =~ %r{server/} }.first || "server/master"
+
+    web_branch = web.split("/").last
+    server_branch = server.split("/").last
+
+    pid = spawn <<-SHELL
+  cd ../server
+  bin/deploy -s #{server_branch} -w #{web_branch}
+  SHELL
+
+    Process.detach(pid)
+
+    "Initializing deployment of #{web} and #{server} to #{environment}."
+  end
+end
